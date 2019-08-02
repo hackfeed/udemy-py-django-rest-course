c27391a2c045a1ea4ecc5e9bfff5254d5d86633d
Sergey Kononenko <hackfeed@yandex.ru>
2019-08-02 22:13:31 +0300

    Add create tag feature

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -64,3 +64,21 @@ class PrivateTagsAPITests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]["name"], tag.name)
+
+    def test_create_tag_succesfull(self):
+        """ Test creating a new tag. """
+        payload = {"name": "Test tag"}
+        self.client.post(TAGS_URL, payload)
+
+        exists = Tag.objects.filter(
+            user=self.user,
+            name=payload["name"]
+        ).exists()
+        self.assertTrue(exists)
+
+    def test_create_tag_invalid(self):
+        """ Test creating new tag with invalid payload. """
+        payload = {"name": ""}
+        res = self.client.post(TAGS_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
